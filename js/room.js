968635da4145cb3d649744acbfc9d385ce0f2300
yunjeong <yunjeong920@naver.com>
2021-05-14 09:13:13 +0900

    스크립트 부분

--- js/room.js
+++ js/room.js
@@ -1,18 +1,90 @@
-// $(function(){
-//   let roomSwiper = new Swiper(".fourthSwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     observeParents: true,
-//     observer: true,
-//     observeSlideChildren: true,
-//     loop: "true",
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev"
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//   });
-// })
+
+$(function(){
+  let firstRoom = new Swiper(".firstSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    observeParents: true,
+    observer: true,
+    observeSlideChildren: true,
+    loop: "true",
+    hashNavigation: {
+      watchState: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+})
+
+$(function(){
+  let secondRoom = new Swiper(".secondSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    observeParents: true,
+    observer: true,
+    observeSlideChildren: true,
+    loop: "true",
+    hashNavigation: {
+      watchState: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+})
+
+
+$(function(){
+  let thirdRoom = new Swiper(".thirdSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    observeParents: true,
+    observer: true,
+    observeSlideChildren: true,
+    hashNavigation: {
+      watchState: true,
+    },
+    loop: "true",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+})
+
+
+$(function(){
+  let fourthRoom = new Swiper(".fourthSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    observeParents: true,
+    observer: true,
+    observeSlideChildren: true,
+    hashNavigation: {
+      watchState: true,
+    },
+    loop: "true",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+})
